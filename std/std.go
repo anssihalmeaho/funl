25e77eed4a68c1ee453f7ba85b480e51274050af
Anssi Halmeaho <anssi.halmeaho@hotmail.com>
2021-02-27 15:40:03 +0200

    Exposed services for adding modules to module cache from Go code

--- std/std.go
+++ std/std.go
@@ -31,6 +31,36 @@ func setSTDFunctions(topFrame *funl.Frame, stdModuleName string, stdFuncs []stdF
 	return
 }
 
+// StdFuncType exposed
+type StdFuncType func(frame *funl.Frame, arguments []funl.Value) (retVal funl.Value)
+
+// StdFuncInfo exposed
+type StdFuncInfo struct {
+	Name       string
+	Getter     func(name string) StdFuncType
+	IsFunction bool
+}
+
+// SetSTDFunctions exposed
+func SetSTDFunctions(topFrame *funl.Frame, stdModuleName string, stdFuncs []StdFuncInfo) (err error) {
+	nsSid := funl.SymIDMap.Add(stdModuleName)
+	funl.GetNSDir().Put(nsSid, topFrame)
+
+	for _, v := range stdFuncs {
+		extProc := funl.ExtProcType{
+			Impl:       v.Getter(stdModuleName + ":" + v.Name),
+			IsFunction: v.IsFunction,
+		}
+		epVal := funl.Value{Kind: funl.ExtProcValue, Data: extProc}
+		item := &funl.Item{Type: funl.ValueItem, Data: epVal}
+		err = topFrame.Syms.Add(v.Name, item)
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
 //InitSTD is used for initializing standard library
 func InitSTD() (err error) {
 	inits := []func() error{
