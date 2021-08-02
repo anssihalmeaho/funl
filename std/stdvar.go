352a3766668a31e894fb66c63ff364a36fe9147f
Anssi Halmeaho <anssi.halmeaho@hotmail.com>
2021-08-02 20:10:52 +0300

    change-v2 added to stdvar

--- std/stdvar.go
+++ std/stdvar.go
@@ -31,6 +31,10 @@ func initSTDVar() (err error) {
 			Name:   "change",
 			Getter: getStdVarChange,
 		},
+		{
+			Name:   "change-v2",
+			Getter: getStdVarChangeV2,
+		},
 	}
 	err = setSTDFunctions(topFrame, stdModuleName, stdFuncs)
 	return
@@ -67,6 +71,121 @@ func (ref *OpaqueVarRef) Equals(with funl.OpaqueAPI) bool {
 	ref.RUnlock()
 	other.RUnlock()
 	return isSame
+}
+
+func getStdVarChangeV2(name string) stdFuncType {
+	return func(frame *funl.Frame, arguments []funl.Value) (retVal funl.Value) {
+		if l := len(arguments); l < 2 {
+			funl.RunTimeError2(frame, "%s: wrong amount of arguments (%d), need two", name, l)
+		}
+		if arguments[0].Kind != funl.OpaqueValue {
+			funl.RunTimeError2(frame, "%s: assuming opaque var-ref", name)
+		}
+		varref, convOK := arguments[0].Data.(*OpaqueVarRef)
+		if !convOK {
+			funl.RunTimeError2(frame, "%s: assuming var-ref", name)
+		}
+
+		switch arguments[1].Kind {
+		case funl.FunctionValue:
+			funcVal, funcOK := arguments[1].Data.(funl.FuncValue)
+			if !funcOK {
+				funl.RunTimeError2(frame, "%s: not func as 2nd argument", name)
+			}
+			if funcVal.FuncProto.IsProc {
+				funl.RunTimeError2(frame, "%s: proc not allowed", name)
+			}
+		case funl.ExtProcValue:
+			extFuncVal, extfuncOK := arguments[1].Data.(funl.ExtProcType)
+			if !extfuncOK {
+				funl.RunTimeError2(frame, "%s: not ext-func as 2nd argument", name)
+			}
+			if !extFuncVal.IsFunction {
+				funl.RunTimeError2(frame, "%s: ext-proc not allowed", name)
+			}
+		default:
+			funl.RunTimeError2(frame, "%s: assuming function as argument", name)
+		}
+
+		varref.Lock()
+		defer varref.Unlock()
+
+		oldVal := *(varref.ValRef)
+
+		argsForCall := []*funl.Item{
+			{
+				Type: funl.ValueItem,
+				Data: arguments[1],
+			},
+			{
+				Type: funl.ValueItem,
+				Data: oldVal,
+			},
+		}
+		if len(arguments) == 3 {
+			argsForCall = append(argsForCall, &funl.Item{Type: funl.ValueItem, Data: arguments[2]})
+		}
+		retListVal, callErr := func() (rv funl.Value, errDesc error) {
+			defer func() {
+				if r := recover(); r != nil {
+					var rtestr string
+					if err, isError := r.(error); isError {
+						rtestr = err.Error()
+					}
+					errDesc = fmt.Errorf("%s", rtestr)
+				}
+			}()
+			return funl.HandleCallOP(frame, argsForCall), nil
+		}()
+
+		var rval funl.Value
+		var addRetVal funl.Value
+		var errtext string
+		if callErr == nil && retListVal.Kind != funl.ListValue {
+			callErr = fmt.Errorf("List value expected")
+		}
+		if callErr == nil {
+			lit := funl.NewListIterator(retListVal)
+			newVal := lit.Next()
+			if newVal == nil {
+				callErr = fmt.Errorf("Too short list received (empty)")
+				errtext = callErr.Error()
+				rval = funl.Value{Kind: funl.StringValue, Data: ""}
+				addRetVal = funl.Value{Kind: funl.StringValue, Data: ""}
+			} else {
+				nextVal := lit.Next()
+				if nextVal == nil {
+					callErr = fmt.Errorf("Too short list received (one item)")
+					errtext = callErr.Error()
+					rval = funl.Value{Kind: funl.StringValue, Data: ""}
+					addRetVal = funl.Value{Kind: funl.StringValue, Data: ""}
+				} else {
+					addRetVal = *nextVal
+					rval = *newVal
+					varref.ValRef = newVal
+				}
+			}
+		} else {
+			errtext = callErr.Error()
+			rval = funl.Value{Kind: funl.StringValue, Data: ""}
+			addRetVal = funl.Value{Kind: funl.StringValue, Data: ""}
+		}
+
+		values := []funl.Value{
+			{
+				Kind: funl.BoolValue,
+				Data: callErr == nil,
+			},
+			{
+				Kind: funl.StringValue,
+				Data: errtext,
+			},
+			rval,
+			addRetVal,
+		}
+		retVal = funl.MakeListOfValues(frame, values)
+		return
+	}
 }
 
 func getStdVarChange(name string) stdFuncType {
@@ -109,11 +228,11 @@ func getStdVarChange(name string) stdFuncType {
 		oldVal := *(varref.ValRef)
 
 		argsForCall := []*funl.Item{
-			&funl.Item{
+			{
 				Type: funl.ValueItem,
 				Data: arguments[1],
 			},
-			&funl.Item{
+			{
 				Type: funl.ValueItem,
 				Data: oldVal,
 			},
---
+++ tst/stdvar_test.fnl
@@ -0,0 +1,41 @@
+
+ns stdvar_test
+
+import ut_fwk
+
+ASSURE = ut_fwk.VERIFY
+
+import common_test_util
+import stdvar
+
+test-change-v2-ok = proc()
+	var = call(stdvar.new 50)
+	retv = call(stdvar.change-v2 var func(prev inp) list(plus(prev inp) list(prev inp)) end 10)
+	call(ASSURE eq(retv list(true, '', 60, list(50, 10))) plus('Unexpected result = ' str(retv)))
+end
+
+test-change-v2-RTE-in-func = proc()
+	var = call(stdvar.new 50)
+	retv = call(stdvar.change-v2 var func(prev inp) list(plus(prev inp) list(prev inp)) end true)
+	call(ASSURE eq(retv list(false, 'mismatching types as arguments', '', '')) plus('Unexpected result = ' str(retv)))
+end
+
+test-change-v2-ret-value-not-list = proc()
+	var = call(stdvar.new 50)
+	retv = call(stdvar.change-v2 var func(prev inp) 'crappy return value' end true)
+	call(ASSURE eq(retv list(false, 'List value expected', '', '')) plus('Unexpected result = ' str(retv)))
+end
+
+test-change-v2-ret-value-is-empty-list = proc()
+	var = call(stdvar.new 50)
+	retv = call(stdvar.change-v2 var func(prev inp) list() end true)
+	call(ASSURE eq(retv list(false, 'Too short list received (empty)', '', '')) plus('Unexpected result = ' str(retv)))
+end
+
+test-change-v2-ret-value-list-too-short = proc()
+	var = call(stdvar.new 50)
+	retv = call(stdvar.change-v2 var func(prev inp) list(1) end true)
+	call(ASSURE eq(retv list(false, 'Too short list received (one item)', '', '')) plus('Unexpected result = ' str(retv)))
+end
+
+endns
